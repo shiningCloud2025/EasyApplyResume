77a3a1384b26d746e1a534be35780b6c84f251b5
shiningCloud2025 <shining_cloud2025@163.com>
2025-11-12 19:53:27 +0800

    RAG实战-SpringAI+云知识库-RAG开发

---
+++ src/main/java/com/zyh/easyapplyresume/demo/rag/AiResumeAssistantRagCloudAdvisorConfig.java
@@ -0,0 +1,38 @@
+package com.zyh.easyapplyresume.demo.rag;
+
+import com.alibaba.cloud.ai.dashscope.api.DashScopeApi;
+import com.alibaba.cloud.ai.dashscope.rag.DashScopeDocumentRetriever;
+import com.alibaba.cloud.ai.dashscope.rag.DashScopeDocumentRetrieverOptions;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.advisor.RetrievalAugmentationAdvisor;
+import org.springframework.ai.chat.client.advisor.api.Advisor;
+import org.springframework.ai.rag.retrieval.search.DocumentRetriever;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * AI简历助手RAG云知识库配置
+ * @author shiningCloud2025
+ */
+@Configuration
+@Slf4j
+public class AiResumeAssistantRagCloudAdvisorConfig {
+
+    @Value("${ali.bailian.api-key}")
+    private String dashScopeApiKey;
+
+    @Bean
+    public Advisor aiResumeAssistantRagCloudAdvisor() {
+        DashScopeApi dashScopeApi = new DashScopeApi(dashScopeApiKey);
+        final String KNOWLEDGE_INDEX = "易投简历";
+        DocumentRetriever documentRetriever = new DashScopeDocumentRetriever(dashScopeApi,
+                DashScopeDocumentRetrieverOptions.builder()
+                        .withIndexName(KNOWLEDGE_INDEX)
+                        .build());
+        return RetrievalAugmentationAdvisor.builder()
+                .documentRetriever(documentRetriever)
+                .build();
+    }
+
+}
