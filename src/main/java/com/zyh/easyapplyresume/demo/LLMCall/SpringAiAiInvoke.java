38816c2c7b8796aa58435b638696802f23c551ab
shiningCloud2025 <shining_cloud2025@163.com>
2025-11-12 09:25:03 +0800

    RAG实战-SpringAI+本地知识库-转换和存储

--- src/main/java/com/zyh/easyapplyresume/demo/LLMCall/SpringAiAiInvoke.java
+++ src/main/java/com/zyh/easyapplyresume/demo/LLMCall/SpringAiAiInvoke.java
@@ -7,17 +7,17 @@ import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-//@Component
-//public class OllamaAiAiInvoke implements CommandLineRunner {
-//    @Resource
-//    private ChatModel dashscopeChatModel;
-//
-//
-//    @Override
-//    public void run(String... args) throws Exception {
-//        AssistantMessage output = dashscopeChatModel.call(new Prompt("你好，我是yunhan"))
-//                .getResult()
-//                .getOutput();
-//        System.out.println(output.getText());
-//    }
-//}
+@Component
+public class SpringAiAiInvoke implements CommandLineRunner {
+    @Resource
+    private ChatModel dashscopeChatModel;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        AssistantMessage output = dashscopeChatModel.call(new Prompt("你好，我是yunhan"))
+                .getResult()
+                .getOutput();
+        System.out.println(output.getText());
+    }
+}
