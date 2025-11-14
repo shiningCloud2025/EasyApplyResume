f9e0283c710b1e657fc28e58dbf47b20b49a970e
shiningCloud2025 <shining_cloud2025@163.com>
2025-11-14 17:22:01 +0800

    RAG实践和调优-编写文档切分器

---
+++ src/main/java/com/zyh/easyapplyresume/demo/rag/MyTokenTextSplitter.java
@@ -0,0 +1,26 @@
+package com.zyh.easyapplyresume.demo.rag;
+
+import org.springframework.ai.document.Document;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * 文档收集和切割-文档切片
+ * @author shiningCloud2025
+ */
+@Component
+public class MyTokenTextSplitter {
+    public List<Document> splitDocuments(List<Document> documents){
+        TokenTextSplitter splitter = new TokenTextSplitter();
+        return splitter.apply( documents);
+    }
+
+    public List<Document> splitCustomized(List<Document> documents){
+        TokenTextSplitter splitter = new TokenTextSplitter(200,100,10,5000,true);
+        return splitter.apply( documents);
+    }
+
+}
+
