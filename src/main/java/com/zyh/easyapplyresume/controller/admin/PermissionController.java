0a99a3c919ea5c5083763959a032c50665cfae5d
shiningCloud2025 <shining_cloud2025@163.com>
2025-11-18 21:25:37 +0800

    管理端-权限接口开发完毕

--- src/main/java/com/zyh/easyapplyresume/controller/admin/PermissionController.java
+++ src/main/java/com/zyh/easyapplyresume/controller/admin/PermissionController.java
@@ -29,26 +29,25 @@ public class PermissionController {
     @Operation(summary = "新增权限")
     @PostMapping("/add")
     public BaseResult<Integer> addPermission(@RequestBody PermissionForm permissionForm){
-
+        return BaseResult.ok(permissionService.addPermission(permissionForm));
     }
 
     @Operation(summary = "修改权限")
     @PostMapping("/update")
     public BaseResult<Integer> updatePermission(@RequestBody PermissionForm permissionForm){
-
+        return BaseResult.ok(permissionService.updatePermission(permissionForm));
     }
 
     @Operation(summary = "删除权限")
     @DeleteMapping("/delete")
     public BaseResult<Integer> deletePermission(@RequestParam(required = true,value = "permissionId") Integer permissionId){
-
-
+        return BaseResult.ok(permissionService.deletePermission(permissionId));
     }
 
     @Operation(summary = "查询权限")
     @GetMapping("/findById")
     public BaseResult<PermissionInfoVO> findPermissionById(@RequestParam(required = true,value = "permissionId") Integer permissionId){
-
+        return BaseResult.ok(permissionService.findPermissionById(permissionId));
     }
 
     @Operation(summary = "分页查询权限")
@@ -56,13 +55,13 @@ public class PermissionController {
     public BaseResult<Page<PermissionPageVO>> findPermissionByPage(@RequestParam(required = false,value = "pageNum",defaultValue = "1") Integer pageNum,
                                                                    @RequestParam(required = false,value = "pageSize",defaultValue = "10") Integer pageSize,
                                                                    @RequestBody PermissionPageQuery permissionPageQuery){
-
+        return BaseResult.ok(permissionService.findPermissionByPage(pageNum,pageSize,permissionPageQuery));
     }
 
     @Operation(summary = "查看所有权限")
     @GetMapping("/findAll")
     public BaseResult<List<PermissionInfoVO>> findAllPermission(){
-
+        return BaseResult.ok(permissionService.findAllPermission());
     }
 
 }
