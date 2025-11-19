4ea2aecae774fb72b9b46cd8c90f847065628ee9
shiningCloud2025 <shining_cloud2025@163.com>
2025-11-19 22:31:22 +0800

    管理端-省份Map控制器

---
+++ src/main/java/com/zyh/easyapplyresume/controller/admin/ProvinceMapController.java
@@ -0,0 +1,38 @@
+package com.zyh.easyapplyresume.controller.admin;
+
+import com.zyh.easyapplyresume.model.pojo.admin.CityMap;
+import com.zyh.easyapplyresume.model.pojo.admin.ProvinceMap;
+import com.zyh.easyapplyresume.service.admin.ProvinceMapService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 省份Map控制器-管理端
+ * @author shiningCloud2025
+ */
+@RestController
+@RequestMapping("/admin/provinceMap")
+@Tag(name="省份Map控制器-管理端")
+public class ProvinceMapController {
+    @Autowired
+    private ProvinceMapService provinceMapService;
+
+    @Operation(summary = "获取所有省份信息")
+    @RequestMapping("/getAllProvince")
+    public List<ProvinceMap> getAllProvince(){
+        return provinceMapService.getAllProvince();
+    }
+
+    @Operation(summary = "根据省份id获取所有市")
+    @RequestMapping("/getCityByProvinceId")
+    public List<CityMap> getCityByProvinceId(Integer provinceMapId){
+        return provinceMapService.getCityByProvinceId(provinceMapId);
+    }
+
+
+}
