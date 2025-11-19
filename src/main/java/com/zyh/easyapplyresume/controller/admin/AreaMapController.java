2f9e45e2c1280b138edf6a3b0ee570d0010e0b43
shiningCloud2025 <shining_cloud2025@163.com>
2025-11-19 22:44:24 +0800

    管理端-区县Map控制器

---
+++ src/main/java/com/zyh/easyapplyresume/controller/admin/AreaMapController.java
@@ -0,0 +1,40 @@
+package com.zyh.easyapplyresume.controller.admin;
+
+import com.zyh.easyapplyresume.model.pojo.admin.AreaMap;
+import com.zyh.easyapplyresume.model.pojo.admin.StreetMap;
+import com.zyh.easyapplyresume.service.admin.AreaMapService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 区县Map控制器-管理端
+ * @author shiningCloud2025
+ */
+@RestController
+@RequestMapping("/admin/areaMap")
+@Tag(name="区县Map控制器-管理端")
+public class AreaMapController {
+    @Autowired
+    private AreaMapService areaMapService;
+
+    @Operation(summary = "获取所有区县信息")
+    @RequestMapping("/getAllArea")
+    public List<AreaMap> getAllArea(){
+        return areaMapService.getAllArea();
+    }
+
+    @Operation(summary = "根据区县id获取所有街道")
+    @RequestMapping("/getStreetByAreaId")
+    public List<StreetMap> getStreetByAreaId(Integer areaMapId){
+        return areaMapService.getStreetByAreaId(areaMapId);
+    }
+
+
+
+
+}
