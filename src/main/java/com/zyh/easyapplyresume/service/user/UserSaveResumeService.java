429a8d20009a402757a63ea886741f7f10321ed4
shiningCloud2025 <shining_cloud2025@163.com>
2025-11-20 23:01:35 +0800

    保存用户简历接口设计

---
+++ src/main/java/com/zyh/easyapplyresume/service/user/UserSaveResumeService.java
@@ -0,0 +1,37 @@
+package com.zyh.easyapplyresume.service.user;
+
+import com.zyh.easyapplyresume.model.pojo.user.UserSaveResume;
+import com.zyh.easyapplyresume.model.vo.user.UserSaveResumeInfoVO;
+
+import java.util.List;
+
+/**
+ * @author shiningCloud2025
+ */
+public interface UserSaveResumeService {
+    /**
+     * 根据用户id查询用户保存的所有简历
+     */
+    public List<UserSaveResumeInfoVO> getUserSaveResumeInfoByUserId(Integer userId);
+
+    /**
+     * 根据用户id和简历id查询用户保存的简历
+     */
+    public UserSaveResumeInfoVO getUserSaveResumeInfoByUserIdAndResumeId(Integer userId, Integer resumeId);
+
+    /**
+     * 根据用户id和简历id删除用户保存的简历
+     */
+    public void deleteUserSaveResumeInfoByUserIdAndResumeId(Integer userId, Integer resumeId);
+
+    /**
+     * 保存用户保存的简历(非第一次添加)
+     */
+    public UserSaveResume saveUserSaveResumeInfo(UserSaveResume userSaveResume);
+
+    /**
+     * 保存用户的简历(第一次添加)
+     */
+    public UserSaveResume saveUserSaveResumeInfoFirst(UserSaveResume userSaveResume);
+
+}
