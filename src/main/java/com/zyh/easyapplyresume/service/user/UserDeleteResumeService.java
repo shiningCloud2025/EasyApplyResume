6309875ff6f931e2ee021e41b0a03f1020247aa9
shiningCloud2025 <shining_cloud2025@163.com>
2025-11-21 11:24:01 +0800

    用户端-用户删除简历设计

--- src/main/java/com/zyh/easyapplyresume/service/user/UserDeleteResumeService.java
+++ src/main/java/com/zyh/easyapplyresume/service/user/UserDeleteResumeService.java
@@ -1,40 +1,40 @@
-//package com.zyh.easyapplyresume.service.user;
-//
-//import com.zyh.easyapplyresume.model.pojo.user.UserDeleteResume;
-//import com.zyh.easyapplyresume.model.pojo.user.UserSaveResume;
-//import com.zyh.easyapplyresume.model.vo.user.UserDeleteResumeInfoVO;
-//import com.zyh.easyapplyresume.model.vo.user.UserSaveResumeInfoVO;
-//
-//import java.util.List;
-//
-///**
-// * @author shiningCloud2025
-// */
-//public interface UserDeleteResumeService {
-//    /**
-//     * 根据用户id查询用户删除的所有简历
-//     */
-//    public List<UserDeleteResumeInfoVO> getUserDeleteResumeInfoByUserId(Integer userId);
-//
-//    /**
-//     * 根据用户id和简历排序查询用户删除的简历
-//     */
-//    public UserDeleteResumeInfoVO getUserDeleteResumeInfoByUserIdAndResumeSortedNum(Integer userId, Integer resumeSortedNum);
-//
-//
-//    /**
-//     * 删除用户保存的简历时，调用这个进行添加
-//     */
-//    public void addUserDeleteSaveResume(UserSaveResumeInfoVO userSaveResumeInfoVO);
-//
-//    /**
-//     * 从用户删除的简历中取出，放回用户简历表
-//     */
-//    public void addUserDeleteResumeToUserSaveResume(UserDeleteResumeInfoVO userDeleteResumeInfoVO);
-//
-//    /**
-//     * 清理过期的简历定时任务，每天早上4点和晚上12点进行一次清理
-//     */
-//    public void clearExpiredResume();
-//
-//}
+package com.zyh.easyapplyresume.service.user;
+
+import com.zyh.easyapplyresume.model.pojo.user.UserDeleteResume;
+import com.zyh.easyapplyresume.model.pojo.user.UserSaveResume;
+import com.zyh.easyapplyresume.model.vo.user.UserDeleteResumeInfoVO;
+import com.zyh.easyapplyresume.model.vo.user.UserSaveResumeInfoVO;
+
+import java.util.List;
+
+/**
+ * @author shiningCloud2025
+ */
+public interface UserDeleteResumeService {
+    /**
+     * 根据用户id查询用户删除的所有简历
+     */
+    public List<UserDeleteResumeInfoVO> getUserDeleteResumeInfoByUserId(Integer userId);
+
+    /**
+     * 根据用户id和简历排序查询用户删除的简历
+     */
+    public UserDeleteResumeInfoVO getUserDeleteResumeInfoByUserIdAndResumeSortedNum(Integer userId, Integer resumeSortedNum);
+
+
+    /**
+     * 删除用户保存的简历时，调用这个进行添加
+     */
+    public void addUserDeleteSaveResume(UserSaveResumeInfoVO userSaveResumeInfoVO);
+
+    /**
+     * 从用户删除的简历中取出，放回用户简历表
+     */
+    public void addUserDeleteResumeToUserSaveResume(UserDeleteResumeInfoVO userDeleteResumeInfoVO);
+
+    /**
+     * 清理过期的简历定时任务，每天早上4点和晚上12点进行一次清理
+     */
+    public void clearExpiredResume();
+
+}
