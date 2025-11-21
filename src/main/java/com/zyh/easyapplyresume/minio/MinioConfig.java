6335ab78c7cc7ed8e55bbdf3036d75b754d38a48
shiningCloud2025 <shining_cloud2025@163.com>
2025-11-21 15:37:45 +0800

    完成Minio的配置

---
+++ src/main/java/com/zyh/easyapplyresume/minio/MinioConfig.java
@@ -0,0 +1,34 @@
+package com.zyh.easyapplyresume.minio;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MinioConfig {
+
+    @Value("${minio.endpoint}")
+    private String endpoint;
+
+    @Value("${minio.accessKey}")
+    private String accessKey;
+
+    @Value("${minio.secretKey}")
+    private String secretKey;
+
+    @Value("${minio.secure}")
+    private boolean secure;
+    /**
+     * 生成 MinioClient Bean，交给 Spring 容器管理。
+     * 这个 Bean 不绑定任何特定的桶，更具通用性。
+     * @return MinioClient 实例
+     */
+    @Bean
+    public MinioClient minioClient(){
+        return MinioClient.builder()
+                .endpoint(endpoint)
+                .credentials(accessKey, secretKey)
+                .build();
+    }
+}
