6be761b6da55afcc0b6caeba31efef901b328b38
shiningCloud2025 <shining_cloud2025@163.com>
2025-11-23 14:03:39 +0800

    IndustryMap接口层开发完毕

--- src/main/java/com/zyh/easyapplyresume/controller/admin/IndustryMapController.java
+++ src/main/java/com/zyh/easyapplyresume/controller/admin/IndustryMapController.java
@@ -1,6 +1,7 @@
 package com.zyh.easyapplyresume.controller.admin;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.zyh.easyapplyresume.bean.usallyexceptionandEnum.BaseResult;
 import com.zyh.easyapplyresume.model.form.admin.IndustryMapForm;
 import com.zyh.easyapplyresume.model.query.admin.IndustryMapQuery;
 import com.zyh.easyapplyresume.model.vo.admin.IndustryMapInfoVO;
@@ -9,7 +10,9 @@ import com.zyh.easyapplyresume.service.admin.IndustryMapService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -28,31 +31,33 @@ public class IndustryMapController {
 
     @Operation(summary = "新增行业Map")
     @RequestMapping("/addIndustryMap")
-    public void addIndustryMap(IndustryMapForm industryMapForm){
-
+    public BaseResult<Integer> addIndustryMap(@RequestBody IndustryMapForm industryMapForm){
+        return BaseResult.ok(industryMapService.addIndustryMap(industryMapForm));
     }
 
     @Operation(summary = "修改行业Map")
     @RequestMapping("/updateIndustryMap")
-    public void updateIndustryMap(IndustryMapForm industryMapForm){
-
+    public BaseResult<Integer>  updateIndustryMap(@RequestBody IndustryMapForm industryMapForm){
+        return BaseResult.ok(industryMapService.updateIndustryMap(industryMapForm));
     }
 
     @Operation(summary = "查询行业Map")
     @RequestMapping("/findIndustryMapById")
-    public IndustryMapInfoVO findIndustryMapById(Integer industryMapId){
-
+    public BaseResult<IndustryMapInfoVO> findIndustryMapById(@RequestParam(required = true,value = "industryMapId") Integer industryMapId){
+        return BaseResult.ok(industryMapService.findIndustryMapById(industryMapId));
     }
 
     @Operation(summary = "分页查询")
     @RequestMapping("/findIndustryMapByPage")
-    public Page<IndustryMapPageVO> findIndustryMapByPage(Integer pageNum, Integer pageSize, IndustryMapQuery industryMapQuery){
-
+    public BaseResult<Page<IndustryMapPageVO>> findIndustryMapByPage(@RequestParam (required = false,value = "pageNum",defaultValue = "1")Integer pageNum,
+                                                                     @RequestParam(required = false,value = "pageSize",defaultValue = "10")  Integer pageSize,
+                                                                     @RequestBody IndustryMapQuery industryMapQuery){
+        return BaseResult.ok(industryMapService.findIndustryMapByPage(pageNum,pageSize,industryMapQuery));
     }
 
     @Operation(summary = "查询所有行业Map")
     @RequestMapping("/findAllIndustryMap")
-    public List<IndustryMapInfoVO> findAllIndustryMap(){
-
+    public BaseResult<List<IndustryMapInfoVO>> findAllIndustryMap(){
+        return BaseResult.ok(industryMapService.findAllIndustryMap());
     }
 }
