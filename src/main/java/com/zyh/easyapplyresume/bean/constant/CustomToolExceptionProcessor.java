c7b8d375703cb0feead41cfe8b524bb6ac0fb823
shiningCloud2025 <shining_cloud2025@163.com>
2025-11-25 13:46:38 +0800

    工具调用异常处理

---
+++ src/main/java/com/zyh/easyapplyresume/bean/constant/CustomToolExceptionProcessor.java
@@ -0,0 +1,10 @@
+package com.zyh.easyapplyresume.bean.constant;
+
+import org.springframework.stereotype.Component;
+/**
+ * 工具调用异常处理--具体逻辑
+ * @author shiningCloud2025
+ */
+@Component
+public class CustomToolExceptionProcessor {
+}
