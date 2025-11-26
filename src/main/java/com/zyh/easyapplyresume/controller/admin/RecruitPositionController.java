2651e533fa8511ba5a6b3784096cf4a1272a9824
shiningCloud2025 <shining_cloud2025@163.com>
2025-11-26 10:14:47 +0800

    招聘岗位接口+服务层实现

--- src/main/java/com/zyh/easyapplyresume/controller/admin/RecruitPositionController.java
+++ src/main/java/com/zyh/easyapplyresume/controller/admin/RecruitPositionController.java
@@ -1,6 +1,7 @@
 package com.zyh.easyapplyresume.controller.admin;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.zyh.easyapplyresume.bean.usallyexceptionandEnum.BaseResult;
 import com.zyh.easyapplyresume.model.form.admin.RecruitPositionForm;
 import com.zyh.easyapplyresume.model.query.admin.RecruitPositionQuery;
 import com.zyh.easyapplyresume.model.vo.admin.RecruitPositionInfoVO;
@@ -26,39 +27,39 @@ public class RecruitPositionController {
 
     @Operation(summary = "新增招聘岗位")
     @PostMapping("/addRecruitPosition")
-    public void addRecruitPosition(@RequestBody RecruitPositionForm recruitPositionForm){
-
+    public BaseResult<?> addRecruitPosition(@RequestBody RecruitPositionForm recruitPositionForm){
+        return BaseResult.ok(recruitPositionService.addRecruitPosition(recruitPositionForm));
     }
     @Operation(summary = "修改招聘岗位")
     @PostMapping("/updateRecruitPosition")
-    public void updateRecruitPosition(@RequestBody RecruitPositionForm recruitPositionForm){
-
+    public  BaseResult<?> updateRecruitPosition(@RequestBody RecruitPositionForm recruitPositionForm){
+        return BaseResult.ok(recruitPositionService.updateRecruitPosition(recruitPositionForm));
     }
 
     @Operation(summary = "删除招聘岗位")
     @DeleteMapping("/deleteRecruitPosition")
-    public void deleteRecruitPosition(@RequestParam(required = true,value = "recruitPositionId") Integer recruitPositionId){
-
+    public  BaseResult<?> deleteRecruitPosition(@RequestParam(required = true,value = "recruitPositionId") Integer recruitPositionId){
+        return BaseResult.ok(recruitPositionService.deleteRecruitPosition(recruitPositionId));
     }
 
     @Operation(summary = "查看招聘岗位")
     @GetMapping("/queryRecruitPosition")
-    public RecruitPositionInfoVO queryRecruitPosition(@RequestParam(required = true,value = "recruitPositionId") Integer recruitPositionId){
-
+    public BaseResult<RecruitPositionInfoVO> queryRecruitPosition(@RequestParam(required = true,value = "recruitPositionId") Integer recruitPositionId){
+        return BaseResult.ok(recruitPositionService.queryRecruitPosition(recruitPositionId));
     }
 
     @Operation(summary = "分页查询招聘岗位")
     @PostMapping("/queryRecruitPositionPage")
-    public Page<RecruitPositionPageVO> queryRecruitPositionPage(@RequestParam(required = false,value = "pageNum",defaultValue = "1") Integer pageNum,
+    public BaseResult<Page<RecruitPositionPageVO>> queryRecruitPositionPage(@RequestParam(required = false,value = "pageNum",defaultValue = "1") Integer pageNum,
                                                                 @RequestParam(required = false,value = "pageSize",defaultValue = "10") Integer pageSize,
                                                                 @RequestBody RecruitPositionQuery recruitPositionQuery){
-
+        return BaseResult.ok(recruitPositionService.queryRecruitPositionPage(pageNum,pageSize,recruitPositionQuery));
     }
 
     @Operation(summary = "查看所有招聘岗位")
     @GetMapping("/queryAllRecruitPositionPage")
-    public List<RecruitPositionInfoVO> queryAllRecruitPositionPage(){
-
+    public BaseResult<List<RecruitPositionInfoVO>> queryAllRecruitPositionPage(){
+        return BaseResult.ok(recruitPositionService.queryAllRecruitPositionPage());
     }
 }
 
