0cfcaf3b7b75a1977929b51d541570abcf9575dd
shiningCloud2025 <shining_cloud2025@163.com>
2025-11-26 09:52:17 +0800

    日志处理+MCP使用

--- src/main/java/com/zyh/easyapplyresume/controller/admin/SendCommunicationEmailController.java
+++ src/main/java/com/zyh/easyapplyresume/controller/admin/SendCommunicationEmailController.java
@@ -3,6 +3,7 @@ package com.zyh.easyapplyresume.controller.admin;
 import com.zyh.easyapplyresume.service.admin.SendCommunicationEmailService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ public class SendCommunicationEmailController {
     private SendCommunicationEmailService sendCommunicationEmailService;
 
     @Operation(summary = "发送纯文本沟通邮件-要指定发送者")
-    @RequestMapping("/selfde/send")
+    @PostMapping("/selfde/sendText")
     public void sendTextEmailSelfDefition(@RequestParam(required = false,value = "fromEmail") String fromEmail,
                                           @RequestParam(required = true,value = "toEmail") String toEmail,
                                           @RequestParam(required = true,value = "subject") String subject,
@@ -27,11 +28,28 @@ public class SendCommunicationEmailController {
     }
 
     @Operation(summary = "发送纯文本沟通邮件-使用配置文件中的默认发送者")
-    @RequestMapping("/usallyde/send")
+    @PostMapping("/usallyde/sendText")
     public void sendTextEmailUsallyDefition(@RequestParam(required = true,value = "toEmail") String toEmail,
                                             @RequestParam(required = true,value = "subject") String subject,
                                             @RequestParam(required = true,value = "content") String content) {
         sendCommunicationEmailService.sendTextEmailUsallyDefition(toEmail, subject, content);
+    }
+
+    @Operation(summary = "发送HTML格式的沟通邮件-要指定发送者")
+    @PostMapping("/selfde/sendHtml")
+    public void sendHtmlEmailSelfDefition(@RequestParam(required = false,value = "fromEmail") String fromEmail,
+                                          @RequestParam(required = true,value = "toEmail") String toEmail,
+                                          @RequestParam(required = true,value = "subject") String subject,
+                                          @RequestParam(required = true,value = "htmlContent") String htmlContent) {
+        sendCommunicationEmailService.sendHtmlEmailSelfDefition(fromEmail, toEmail, subject, htmlContent);
+    }
+
+    @Operation(summary = "发送HTML格式的沟通邮件-使用配置文件中的默认发送者")
+    @PostMapping("/usallyde/sendHtml")
+    public void sendHtmlEmailUsallyDefition(@RequestParam(required = true,value = "toEmail") String toEmail,
+                                            @RequestParam(required = true,value = "subject") String subject,
+                                            @RequestParam(required = true,value = "htmlContent") String htmlContent) {
+        sendCommunicationEmailService.sendHtmlEmailUsallyDefition(toEmail, subject, htmlContent);
     }
 
 
--- src/main/java/com/zyh/easyapplyresume/demo/app/AiResumeAssistant.java
+++ src/main/java/com/zyh/easyapplyresume/demo/app/AiResumeAssistant.java
@@ -21,6 +21,7 @@ import org.springframework.ai.chat.memory.InMemoryChatMemory;
 import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.ai.tool.ToolCallback;
+import org.springframework.ai.tool.ToolCallbackProvider;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -179,6 +180,24 @@ public class AiResumeAssistant {
                         .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY,10))
                 .advisors(new MyLoggerAdvisor())
                 .tools(allTools)
+                .call()
+                .chatResponse();
+        String content = chatResponse.getResult().getOutput().getText();
+        log.info("content:{}",content);
+        return  content;
+    }
+
+    @Resource
+    private ToolCallbackProvider toolCallbackProvider;
+
+    public String doChatWithMcp(String message,String chatId){
+        ChatResponse chatResponse = chatClient
+                .prompt()
+                .user(message)
+                .advisors(spec->spec.param(CHAT_MEMORY_CONVERSATION_ID_KEY,chatId)
+                        .param(CHAT_MEMORY_RETRIEVE_SIZE_KEY,10))
+                .advisors(new MyLoggerAdvisor())
+                .tools(toolCallbackProvider)
                 .call()
                 .chatResponse();
         String content = chatResponse.getResult().getOutput().getText();
