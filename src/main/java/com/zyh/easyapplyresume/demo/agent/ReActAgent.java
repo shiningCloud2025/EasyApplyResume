ce855f00c0d8c6dc61bad966eff7315cb778192d
shiningCloud2025 <shining_cloud2025@163.com>
2025-11-28 20:45:50 +0800

    Agent-ReActAgent

--- src/main/java/com/zyh/easyapplyresume/demo/agent/ReActAgent.java
+++ src/main/java/com/zyh/easyapplyresume/demo/agent/ReActAgent.java
@@ -1,38 +1,38 @@
-//package com.zyh.easyapplyresume.demo.agent;
-//
-//import lombok.Data;
-//import lombok.extern.slf4j.Slf4j;
-//
-///**
-// * ReAct模式的代理抽象类
-// * 实现了思考-行动的循环模式
-// * @author shiningCloud2025
-// */
-//@Data
-//public abstract class ReActAgent extends BaseAgent{
-//    /**
-//     * 处理当前状态并决定下一步行动
-//     * @return 是否需要执行行动,true标识需要执行，false表示不需要执行
-//     */
-//    public abstract boolean think();
-//
-//    /**
-//     * 执行决定的行动
-//     * @return
-//     */
-//    public abstract String act();
-//
-//    @Override
-//    public String step(){
-//        try{
-//            boolean shouldAct = think();
-//            if (!shouldAct){
-//                return "思考完成-无需行动";
-//            }
-//            return act();
-//        }catch (Exception e){
-//            e.printStackTrace();
-//            return "步骤执行失败: "+e.getMessage();
-//        }
-//    }
-//}
+package com.zyh.easyapplyresume.demo.agent;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * ReAct模式的代理抽象类
+ * 实现了思考-行动的循环模式
+ * @author shiningCloud2025
+ */
+@Data
+public abstract class ReActAgent extends BaseAgent{
+    /**
+     * 处理当前状态并决定下一步行动
+     * @return 是否需要执行行动,true标识需要执行，false表示不需要执行
+     */
+    public abstract boolean think();
+
+    /**
+     * 执行决定的行动
+     * @return
+     */
+    public abstract String act();
+
+    @Override
+    public String step(){
+        try{
+            boolean shouldAct = think();
+            if (!shouldAct){
+                return "思考完成-无需行动";
+            }
+            return act();
+        }catch (Exception e){
+            e.printStackTrace();
+            return "步骤执行失败: "+e.getMessage();
+        }
+    }
+}
