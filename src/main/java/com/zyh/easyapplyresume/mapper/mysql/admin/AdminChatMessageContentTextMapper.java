b7b09d7f538f9a4366ff9864527a52907de0c9a2
shiningCloud2025 <shining_cloud2025@163.com>
2025-11-29 22:37:36 +0800

    完成客户端对话存储实体类

---
+++ src/main/java/com/zyh/easyapplyresume/mapper/mysql/admin/AdminChatMessageContentTextMapper.java
@@ -0,0 +1,10 @@
+package com.zyh.easyapplyresume.mapper.mysql.admin;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zyh.easyapplyresume.model.pojo.admin.AdminChatMessageContentText;
+
+/**
+ * @author shiningCloud2025
+ */
+public interface AdminChatMessageContentTextMapper extends BaseMapper<AdminChatMessageContentText> {
+}
