04df9d058f3b9b76bf95a2d007874b507d2b0370
shiningCloud2025 <shining_cloud2025@163.com>
2025-11-30 17:51:21 +0800

    用户端过滤器

---
+++ src/main/java/com/zyh/easyapplyresume/mapper/mysql/user/UserMapper.java
@@ -0,0 +1,7 @@
+package com.zyh.easyapplyresume.mapper.mysql.user;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zyh.easyapplyresume.model.pojo.user.User;
+
+public interface UserMapper extends BaseMapper<User> {
+}
