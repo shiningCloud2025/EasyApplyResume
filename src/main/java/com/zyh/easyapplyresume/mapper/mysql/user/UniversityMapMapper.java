2ef710c41fb5084e19797caeb7555b8c6bbed663
shiningCloud2025 <shining_cloud2025@163.com>
2025-12-01 21:48:28 +0800

    完成用户端mapper层开发

--- src/main/java/com/zyh/easyapplyresume/mapper/mysql/user/UniversityMapMapper.java
+++ src/main/java/com/zyh/easyapplyresume/mapper/mysql/user/UniversityMapMapper.java
@@ -1,9 +1,10 @@
 package com.zyh.easyapplyresume.mapper.mysql.user;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.zyh.easyapplyresume.model.pojo.user.User;
+import com.zyh.easyapplyresume.model.pojo.user.UniversityMap;
+
 /**
  * @author shiningCloud2025
  */
-public interface UserMapper extends BaseMapper<User> {
+public interface UniversityMapMapper extends BaseMapper<UniversityMap> {
 }
