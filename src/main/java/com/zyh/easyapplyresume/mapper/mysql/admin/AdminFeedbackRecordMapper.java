3da4bcb159b61d9d22115d0a628617e967231caf
shiningCloud2025 <shining_cloud2025@163.com>
2025-12-06 20:39:05 +0800

    反馈双端Mapper构建完成

---
+++ src/main/java/com/zyh/easyapplyresume/mapper/mysql/admin/AdminFeedbackRecordMapper.java
@@ -0,0 +1,10 @@
+package com.zyh.easyapplyresume.mapper.mysql.admin;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zyh.easyapplyresume.model.pojo.admin.AdminFeedbackRecord;
+
+/**
+ * @author shiningCloud2025
+ */
+public interface AdminFeedbackRecordMapper extends BaseMapper<AdminFeedbackRecord> {
+}
---
+++ src/main/java/com/zyh/easyapplyresume/mapper/mysql/user/UserFeedbackMapper.java
@@ -0,0 +1,10 @@
+package com.zyh.easyapplyresume.mapper.mysql.user;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zyh.easyapplyresume.model.pojo.user.UserFeedback;
+
+/**
+ * @author shiningCloud2025
+ */
+public interface UserFeedbackMapper extends BaseMapper<UserFeedback> {
+}
