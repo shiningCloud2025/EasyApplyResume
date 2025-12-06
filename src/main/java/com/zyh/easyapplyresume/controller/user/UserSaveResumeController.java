0a91d9bd5c5fe6070d6195cc86c104c5055a26f2
shiningCloud2025 <shining_cloud2025@163.com>
2025-12-06 20:03:59 +0800

    反馈管理端VO完成

--- src/main/java/com/zyh/easyapplyresume/controller/user/UserSaveResumeController.java
+++ src/main/java/com/zyh/easyapplyresume/controller/user/UserSaveResumeController.java
@@ -1,4 +1,19 @@
 package com.zyh.easyapplyresume.controller.user;
 
+import com.zyh.easyapplyresume.service.user.UserSaveResumeService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user/saveResume")
+@Tag(name="用户保存简历控制器-用户端")
 public class UserSaveResumeController {
+
+    @Autowired
+    private UserSaveResumeService userSaveResumeService;
+
+
+
 }
