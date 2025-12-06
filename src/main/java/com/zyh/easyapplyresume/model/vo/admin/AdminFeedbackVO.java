197534677b87000e037eeb40fd8c97db3d198cdf
shiningCloud2025 <shining_cloud2025@163.com>
2025-12-06 20:03:59 +0800

    反馈管理端VO完成

---
+++ src/main/java/com/zyh/easyapplyresume/model/vo/admin/AdminFeedbackVO.java
@@ -0,0 +1,38 @@
+package com.zyh.easyapplyresume.model.vo.admin;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * @author shiningCloud2025
+ */
+@Data
+@Schema(description = "管理员反馈信息")
+public class AdminFeedbackVO {
+    
+    @Schema(description = "管理员反馈ID")
+    private Integer adminFeedbackId;
+    
+    @Schema(description = "反馈标题")
+    private String adminFeedbackTitle;
+    
+    @Schema(description = "反馈详细内容")
+    private String adminFeedbackContent;
+    
+    @Schema(description = "反馈提交时间")
+    private Date adminFeedbackTime;
+    
+    @Schema(description = "最近一次处理时间")
+    private Date adminFeedbackRecentTime;
+    
+    @Schema(description = "反馈现阶段")
+    private String adminFeedbackCurStep;
+    
+    @Schema(description = "提交反馈的管理员ID")
+    private Integer adminFeedbackAdminId;
+    
+    @Schema(description = "提交反馈的管理员姓名")
+    private String adminFeedbackAdminName;
+}
