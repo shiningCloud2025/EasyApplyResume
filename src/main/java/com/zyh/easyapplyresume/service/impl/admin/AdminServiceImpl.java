67ce4882e9decc76f17ab4985f2cfa06ba81191f
shiningCloud2025 <shining_cloud2025@163.com>
2025-12-07 16:14:08 +0800

    管理员业务修改

--- src/main/java/com/zyh/easyapplyresume/service/impl/admin/AdminServiceImpl.java
+++ src/main/java/com/zyh/easyapplyresume/service/impl/admin/AdminServiceImpl.java
@@ -169,6 +169,27 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public String generateRandomAccount() {
-        return "";
+        String account;
+        int maxRetries = 10;
+        int retryCount = 0;
+        
+        do {
+            long timestamp = System.currentTimeMillis() % 100000000L;
+            int randomNum = (int) (Math.random() * 100);
+            account = String.format("%d%02d", timestamp, randomNum);
+            
+            if (account.length() > 10) {
+                account = account.substring(account.length() - 10);
+            } else if (account.length() < 7) {
+                account = String.format("%07d", Long.parseLong(account));
+            }
+            
+            retryCount++;
+            if (retryCount > maxRetries) {
+                throw new BusException(AdminCodeEnum.GENERATE_ACCOUNT_FAIL);
+            }
+        } while (adminMapper.findByAccountOrPhoneOrEmail(account) != null);
+        
+        return account;
     }
 }
