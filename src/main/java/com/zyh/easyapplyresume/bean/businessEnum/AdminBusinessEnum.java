3f53901bcff584d7110c861960aad4e5d3131ebe
shiningCloud2025 <shining_cloud2025@163.com>
2025-12-07 13:58:28 +0800

    反馈基本业务

--- src/main/java/com/zyh/easyapplyresume/bean/businessEnum/AdminBusinessEnum.java
+++ src/main/java/com/zyh/easyapplyresume/bean/businessEnum/AdminBusinessEnum.java
@@ -5,9 +5,27 @@ import lombok.Getter;
 
 /**
  * 管理端业务枚举类
- * @author shiningCloudEu
+ * @author shiningCloud2025
  */
 @Getter
 @AllArgsConstructor
 public enum AdminBusinessEnum {
+
+    //====================== 管理端反馈业务枚举类 ======================
+    ADMIN_WAIT_RECEIVED(1, "待接受"),
+    ADMIN_DO_RECEIVER(2, "待回复"),
+    ADMIN_ALREADY_IGNORE(3, "已忽视"),
+    ADMIN_ALREADY_REPLY(4, "已回复"),
+    ADMIN_REJECT_REPLY(5, "已拒绝回复")
+
+
+
+
+
+
+
+
+    ;
+    private final Integer code;
+    private final String message;
 }
--- src/main/java/com/zyh/easyapplyresume/bean/businessEnum/UserBusinessEnum.java
+++ src/main/java/com/zyh/easyapplyresume/bean/businessEnum/UserBusinessEnum.java
@@ -1,4 +1,28 @@
 package com.zyh.easyapplyresume.bean.businessEnum;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * 用户端业务枚举类
+ * @author shiningCloud2025
+ */
+@Getter
+@AllArgsConstructor
 public enum UserBusinessEnum {
+
+    //====================== 用户端反馈业务枚举类 ======================
+    USER_WAIT_RECEIVED(1, "待接受"),
+    USER_DO_RECEIVER(2, "待回复"),
+    USER_ALREADY_IGNORE(3, "已忽视"),
+    USER_ALREADY_REPLY(4, "已回复"),
+    USER_REJECT_REPLY(5, "已拒绝回复")
+
+
+
+
+
+    ;
+    private final Integer code;
+    private final String message;
 }
