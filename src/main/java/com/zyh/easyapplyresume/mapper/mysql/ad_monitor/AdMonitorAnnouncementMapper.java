c15c3b93f114c067df8311b5373606c391ab202d
shiningCloud2025 <shining_cloud2025@163.com>
2025-12-13 22:56:34 +0800

    公告业务完成业务层接口设计

---
+++ src/main/java/com/zyh/easyapplyresume/mapper/mysql/ad_monitor/AdMonitorAnnouncementMapper.java
@@ -0,0 +1,10 @@
+package com.zyh.easyapplyresume.mapper.mysql.ad_monitor;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zyh.easyapplyresume.model.pojo.ad_monitor.AdMonitorAnnouncement;
+
+/**
+ * @author shiningCloud2025
+ */
+public interface AdMonitorAnnouncementMapper extends BaseMapper<AdMonitorAnnouncement> {
+}
---
+++ src/main/java/com/zyh/easyapplyresume/mapper/mysql/admin/AdminAnnouncementMapper.java
@@ -0,0 +1,10 @@
+package com.zyh.easyapplyresume.mapper.mysql.admin;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.zyh.easyapplyresume.model.pojo.admin.AdminAnnouncement;
+
+/**
+ * @author shiningCloud2025
+ */
+public interface AdminAnnouncementMapper extends BaseMapper<AdminAnnouncement> {
+}
