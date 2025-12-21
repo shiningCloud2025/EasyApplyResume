018b88a3ae56437771cb0972acbd085cafdad456
shiningCloud2025 <shining_cloud2025@163.com>
2025-12-21 14:07:14 +0800

    发送简历服务层做好了

---
+++ src/main/java/com/zyh/easyapplyresume/controller/user/UserSendResumeToHrController.java
@@ -0,0 +1,14 @@
+package com.zyh.easyapplyresume.controller.user;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * 用户发送简历给HR控制器-用户端
+ * @author shiningCloud2025
+ */
+@RestController
+@RequestMapping("/user/sendResumeToHr")
+@Tag(name = "用户发送简历给HR")
+public class UserSendResumeToHrController {
+}
