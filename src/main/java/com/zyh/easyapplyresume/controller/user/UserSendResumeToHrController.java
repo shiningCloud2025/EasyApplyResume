1ecb6446d90eea8e8fc059a9750156e125ae9164
shiningCloud2025 <shining_cloud2025@163.com>
2025-12-21 14:09:12 +0800

    发送简历功能开发完毕

--- src/main/java/com/zyh/easyapplyresume/controller/user/UserSendResumeToHrController.java
+++ src/main/java/com/zyh/easyapplyresume/controller/user/UserSendResumeToHrController.java
@@ -1,6 +1,13 @@
 package com.zyh.easyapplyresume.controller.user;
 
+import com.zyh.easyapplyresume.bean.usallyexceptionandEnum.BaseResult;
+import com.zyh.easyapplyresume.model.form.user.UserSendResumeToHrForm;
+import com.zyh.easyapplyresume.service.user.UserSendResumeToHrService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 /**
@@ -11,4 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/user/sendResumeToHr")
 @Tag(name = "用户发送简历给HR")
 public class UserSendResumeToHrController {
+
+    @Autowired
+    private UserSendResumeToHrService userSendResumeToHrService;
+
+    @PostMapping("/sendResumeToHr")
+    @Operation(summary = "用户发送简历给HR")
+    public BaseResult<?> sendResumeToHr(@RequestBody UserSendResumeToHrForm userSendResumeToHrForm) {
+        userSendResumeToHrService.sendResumeToHr(userSendResumeToHrForm);
+        return BaseResult.ok();
+    }
+
 }
