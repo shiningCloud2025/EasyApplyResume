e6e225e2d9255b335fd0cafaeabe35afe46274f4
shiningCloud2025 <shining_cloud2025@163.com>
2025-12-22 21:06:04 +0800

    用户业务服务层业务开发完毕

--- src/main/java/com/zyh/easyapplyresume/service/impl/user/UserAuthServiceImpl.java
+++ src/main/java/com/zyh/easyapplyresume/service/impl/user/UserAuthServiceImpl.java
@@ -1,5 +1,6 @@
 package com.zyh.easyapplyresume.service.impl.user;
 
+import cn.hutool.core.date.DateUtil;
 import com.zyh.easyapplyresume.bean.locationenum.CityEnum;
 import com.zyh.easyapplyresume.bean.locationenum.ProvinceEnum;
 import com.zyh.easyapplyresume.bean.usallyexceptionandEnum.BusException;
@@ -9,7 +10,10 @@ import com.zyh.easyapplyresume.model.form.user.EmailLoginForm;
 import com.zyh.easyapplyresume.model.form.user.FormalLoginForm;
 import com.zyh.easyapplyresume.model.form.user.FormalRegisterForm;
 import com.zyh.easyapplyresume.model.form.user.PhoneLoginForm;
+import com.zyh.easyapplyresume.model.pojo.ad_monitor.AdmonitorAdminDailyVisitNum;
+import com.zyh.easyapplyresume.model.pojo.ad_monitor.AdmonitorUserDailyVisitNum;
 import com.zyh.easyapplyresume.model.pojo.user.User;
+import com.zyh.easyapplyresume.service.ad_monitor.AdmonitorUserDailyVisitNumService;
 import com.zyh.easyapplyresume.service.user.UserAuthService;
 import com.zyh.easyapplyresume.service.user.UserLoginAndRegisterEmailVerifyService;
 import com.zyh.easyapplyresume.service.user.UserSmsService;
@@ -59,6 +63,9 @@ public class UserAuthServiceImpl implements UserAuthService {
     @Autowired
     private UserLoginAndRegisterEmailVerifyService userLoginAndRegisterEmailVerifyService;
 
+    @Autowired
+    private AdmonitorUserDailyVisitNumService admonitorUserDailyVisitNumService;
+
     /**
      * 普通登录(账号/手机号/邮箱号)
      * @param formalLoginForm
@@ -72,6 +79,14 @@ public class UserAuthServiceImpl implements UserAuthService {
         }
         if (!passwordEncoder.matches(formalLoginForm.getPassword(), user.getUserPassword())){
             throw new BusException(UserCodeEnum.ACCOUNT_OR_PASSWORD_ERROR);
+        }
+        if (user.getUserLoginTime() == null || !DateUtil.isSameDay(user.getUserLoginTime(),new Date())){
+            // TODO:这里不能用构造 因为构造没有自动类型转换
+            AdmonitorUserDailyVisitNum admonitorUserDailyVisitNum = new AdmonitorUserDailyVisitNum();
+            admonitorUserDailyVisitNum.setUserDailyVisitNumAdminId(user.getUserId());
+            admonitorUserDailyVisitNum.setUserDailyVisitNumVisitTime(new Date());
+            admonitorUserDailyVisitNumService.addAdmonitorUserDailyVisitNum(admonitorUserDailyVisitNum);
+
         }
         user.setUserLoginTime(new Date());
         userMapper.updateById(user);
@@ -93,6 +108,14 @@ public class UserAuthServiceImpl implements UserAuthService {
         if (user== null){
             throw new BusException(UserCodeEnum.NO_REGISTER_ERROR);
         }
+        if (user.getUserLoginTime() == null || !DateUtil.isSameDay(user.getUserLoginTime(),new Date())){
+            // TODO:这里不能用构造 因为构造没有自动类型转换
+            AdmonitorUserDailyVisitNum admonitorUserDailyVisitNum = new AdmonitorUserDailyVisitNum();
+            admonitorUserDailyVisitNum.setUserDailyVisitNumAdminId(user.getUserId());
+            admonitorUserDailyVisitNum.setUserDailyVisitNumVisitTime(new Date());
+            admonitorUserDailyVisitNumService.addAdmonitorUserDailyVisitNum(admonitorUserDailyVisitNum);
+
+        }
         user.setUserLoginTime(new Date());
         userMapper.updateById(user);
         String token = jwtUtil.generateToken(user.getUserId(), user.getUserUsername(), "user", jwtSecret, jwtExpiration);
@@ -112,6 +135,14 @@ public class UserAuthServiceImpl implements UserAuthService {
         User user = userMapper.findByAccountOrPhoneOrEmail(emailLoginForm.getEmail());
         if (user== null){
             throw new BusException(UserCodeEnum.NO_REGISTER_ERROR);
+        }
+        if (user.getUserLoginTime() == null || !DateUtil.isSameDay(user.getUserLoginTime(),new Date())){
+            // TODO:这里不能用构造 因为构造没有自动类型转换
+            AdmonitorUserDailyVisitNum admonitorUserDailyVisitNum = new AdmonitorUserDailyVisitNum();
+            admonitorUserDailyVisitNum.setUserDailyVisitNumAdminId(user.getUserId());
+            admonitorUserDailyVisitNum.setUserDailyVisitNumVisitTime(new Date());
+            admonitorUserDailyVisitNumService.addAdmonitorUserDailyVisitNum(admonitorUserDailyVisitNum);
+
         }
         user.setUserLoginTime(new Date());
         userMapper.updateById(user);
