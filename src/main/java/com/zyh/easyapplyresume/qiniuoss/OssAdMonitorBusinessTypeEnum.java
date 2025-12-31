c82d21e69d064d8823d90b144563062e1a1a244c
zhaoyunhan1 <zhaoyunhan1@company.local>
2025-12-31 19:53:55 +0800

    完成OSS,三端分离、用户分离、业务分离

---
+++ src/main/java/com/zyh/easyapplyresume/qiniuoss/OssAdMonitorBusinessTypeEnum.java
@@ -0,0 +1,15 @@
+package com.zyh.easyapplyresume.qiniuoss;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+/**
+ * 监测端OSS业务类型枚举类
+ * @author shiningCloud2025
+ */
+@Getter
+@AllArgsConstructor
+public enum OssAdMonitorBusinessTypeEnum implements OssBusinessType{
+
+    ;
+    private final String dir;
+}
