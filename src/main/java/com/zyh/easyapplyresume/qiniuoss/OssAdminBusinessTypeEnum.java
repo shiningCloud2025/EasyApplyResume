2db21f3e81680e64e4bb61f2532ffe882bb01474
zhaoyunhan1 <zhaoyunhan1@company.local>
2025-12-31 19:38:19 +0800

    完成OssSystemTypeEnum:三端隔离

---
+++ src/main/java/com/zyh/easyapplyresume/qiniuoss/OssAdminBusinessTypeEnum.java
@@ -0,0 +1,16 @@
+package com.zyh.easyapplyresume.qiniuoss;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+/**
+ * 管理端OSS业务类型枚举类
+ * @author shiningCloud2025
+ */
+@Getter
+@AllArgsConstructor
+public enum OssAdminBusinessTypeEnum implements OssBusinessType{
+
+    ;
+    private final String dir;
+
+}
