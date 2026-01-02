d4b5723c6a9c3b14df0a5b7ffe29b6ee6f0f2d95
zhaoyunhan1 <zhaoyunhan1@company.local>
2026-01-02 19:52:28 +0800

    服务器设备管理

---
+++ src/main/java/com/zyh/easyapplyresume/controller/ad_monitor/AdmonitorServiceMachineManageController.java
@@ -0,0 +1,60 @@
+package com.zyh.easyapplyresume.controller.ad_monitor;
+
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.zyh.easyapplyresume.bean.usallyexceptionandEnum.BaseResult;
+import com.zyh.easyapplyresume.model.form.ad_monitor.AdmonitorServiceMachineForm;
+import com.zyh.easyapplyresume.model.query.ad_monitor.AdmonitorServiceMachineQuery;
+import com.zyh.easyapplyresume.model.vo.ad_monitor.AdmonitorServiceMachineInfoVO;
+import com.zyh.easyapplyresume.model.vo.ad_monitor.AdmonitorServiceMachinePageVO;
+import com.zyh.easyapplyresume.service.ad_monitor.AdmonitorServiceMachineService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * 服务器设备管理-监控端
+ * @author shiningCloudEth
+ */
+@RestController
+@RequestMapping("/admonitor/servicemachine/manage")
+@Tag(name = "服务器设备管理-监控端")
+public class AdmonitorServiceMachineManageController {
+
+    @Autowired
+    private AdmonitorServiceMachineService admonitorServiceMachineService;
+
+    @PostMapping("/addAdmonitorServiceMachine")
+    @Operation(summary = "添加服务器设备")
+    public BaseResult<Integer> addAdmonitorServiceMachine(@RequestBody AdmonitorServiceMachineForm admonitorServiceMachineForm) {
+        return BaseResult.ok(admonitorServiceMachineService.addAdmonitorServiceMachine(admonitorServiceMachineForm));
+    }
+
+    @PutMapping("/updateAdmonitorServiceMachine")
+    @Operation(summary = "更新服务器设备")
+    public BaseResult<Integer> updateAdmonitorServiceMachine(@RequestBody AdmonitorServiceMachineForm admonitorServiceMachineForm) {
+        return BaseResult.ok(admonitorServiceMachineService.updateAdmonitorServiceMachine(admonitorServiceMachineForm));
+    }
+
+    @DeleteMapping("/deleteAdmonitorServiceMachine")
+    @Operation(summary = "删除服务器设备")
+    public BaseResult<Integer> deleteAdmonitorServiceMachine(@RequestParam(required = true, name = "id") Integer id) {
+        return BaseResult.ok(admonitorServiceMachineService.deleteAdmonitorServiceMachine(id));
+    }
+
+    @GetMapping("/getAdmonitorServiceMachineInfo")
+    @Operation(summary = "获取服务器设备信息")
+    public BaseResult<AdmonitorServiceMachineInfoVO> getAdmonitorServiceMachineInfo(@RequestParam(required = true, name = "id") Integer id) {
+        return BaseResult.ok(admonitorServiceMachineService.getAdmonitorServiceMachineInfo(id));
+    }
+
+    @PostMapping("/getAdmonitorServiceMachinePage")
+    @Operation(summary = "获取服务器设备分页")
+    public BaseResult<Page<AdmonitorServiceMachinePageVO>> getAdmonitorServiceMachinePage(@RequestParam(required = true, name = "pageNum") Integer pageNum,
+                                                                                          @RequestParam(required = true, name = "pageSize") Integer pageSize,
+                                                                                          @RequestBody AdmonitorServiceMachineQuery admonitorServiceMachineQuery) {
+        return BaseResult.ok(admonitorServiceMachineService.getAdmonitorServiceMachinePage(pageNum, pageSize, admonitorServiceMachineQuery));
+                                                                                          }
+
+
+}
