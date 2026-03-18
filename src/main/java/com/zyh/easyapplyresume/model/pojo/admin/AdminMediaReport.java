88f6916284895ef952fad7a497d59f542e84123f
shiningCloud2025 <shining_cloud2025@163.com>
2026-03-19 00:19:22 +0800

    九大业务模块-模型层-媒体报道改时间类型

--- src/main/java/com/zyh/easyapplyresume/model/pojo/admin/AdminMediaReport.java
+++ src/main/java/com/zyh/easyapplyresume/model/pojo/admin/AdminMediaReport.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 /**
@@ -38,5 +39,5 @@ public class AdminMediaReport {
      * 修改时间
      */
     @TableField("mediaReport_updatedTime")
-    private Date mediaReportUpdatedTime;
+    private LocalDateTime mediaReportUpdatedTime;
 }
