827b14f1691636d4db9f2ae9b61144e5b58c7607
shiningCloud2025 <shining_cloud2025@163.com>
2026-03-19 00:25:15 +0800

    九大业务模块-VO层-加入我们改时间类型

--- src/main/java/com/zyh/easyapplyresume/model/vo/admin/AdminJoinUsInfoVO.java
+++ src/main/java/com/zyh/easyapplyresume/model/vo/admin/AdminJoinUsInfoVO.java
@@ -3,6 +3,7 @@ package com.zyh.easyapplyresume.model.vo.admin;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 /**
@@ -21,5 +22,5 @@ public class AdminJoinUsInfoVO {
     private String joinUsContent;
 
     @Schema(description = "加入我们更新时间")
-    private Date joinUsUpdatedTime;
+    private LocalDateTime joinUsUpdatedTime;
 }
