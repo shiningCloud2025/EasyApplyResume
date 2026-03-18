74f0f97c74922bbac387acec2258a465e6ea01fd
shiningCloud2025 <shining_cloud2025@163.com>
2026-03-19 00:21:21 +0800

    九大业务模块-模型层-团队介绍改时间类型

--- src/main/java/com/zyh/easyapplyresume/model/pojo/admin/AdminTeamIntroduce.java
+++ src/main/java/com/zyh/easyapplyresume/model/pojo/admin/AdminTeamIntroduce.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 /**
@@ -38,5 +39,5 @@ public class AdminTeamIntroduce {
      * 修改时间
      */
     @TableField("teamIntroduce_updatedTime")
-    private Date teamIntroduceUpdatedTime;
+    private LocalDateTime teamIntroduceUpdatedTime;
 }
