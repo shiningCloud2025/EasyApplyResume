20d74b766dc90736018ca63a8f7b3137b969be6a
shiningCloud2025 <shining_cloud2025@163.com>
2026-03-19 00:23:01 +0800

    九大业务模块-VO层-发展历程改时间类型

--- src/main/java/com/zyh/easyapplyresume/model/vo/admin/AdminDevelopHistoryInfoVO.java
+++ src/main/java/com/zyh/easyapplyresume/model/vo/admin/AdminDevelopHistoryInfoVO.java
@@ -3,6 +3,7 @@ package com.zyh.easyapplyresume.model.vo.admin;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 /**
@@ -21,5 +22,5 @@ public class AdminDevelopHistoryInfoVO {
     private String developHistoryContent;
 
     @Schema(description = "发展历程更新时间")
-    private Date developHistoryUpdatedTime;
+    private LocalDateTime developHistoryUpdatedTime;
 }
