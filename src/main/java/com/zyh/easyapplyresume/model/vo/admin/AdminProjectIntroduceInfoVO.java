e59e9199b88b3b3e1924a121973f6ca304e3a3b0
shiningCloud2025 <shining_cloud2025@163.com>
2026-03-19 00:27:06 +0800

    九大业务模块-VO层-项目介绍改时间类型

--- src/main/java/com/zyh/easyapplyresume/model/vo/admin/AdminProjectIntroduceInfoVO.java
+++ src/main/java/com/zyh/easyapplyresume/model/vo/admin/AdminProjectIntroduceInfoVO.java
@@ -3,6 +3,7 @@ package com.zyh.easyapplyresume.model.vo.admin;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 /**
@@ -21,5 +22,5 @@ public class AdminProjectIntroduceInfoVO {
     private String projectIntroduceContent;
 
     @Schema(description = "项目介绍更新时间")
-    private Date projectIntroduceUpdatedTime;
+    private LocalDateTime projectIntroduceUpdatedTime;
 }
