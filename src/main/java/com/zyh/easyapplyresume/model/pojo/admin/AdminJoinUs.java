c90a290cfe5c23a207c43260e77509bd3b1ee589
shiningCloud2025 <shining_cloud2025@163.com>
2026-03-19 00:18:46 +0800

    九大业务模块-模型层-加入我们改时间类型

--- src/main/java/com/zyh/easyapplyresume/model/pojo/admin/AdminJoinUs.java
+++ src/main/java/com/zyh/easyapplyresume/model/pojo/admin/AdminJoinUs.java
@@ -8,6 +8,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
 import java.util.Date;
 
 /**
@@ -38,5 +39,5 @@ public class AdminJoinUs {
      * 修改时间
      */
     @TableField("joinUs_updatedTime")
-    private Date joinUsUpdatedTime;
+    private LocalDateTime joinUsUpdatedTime;
 }
